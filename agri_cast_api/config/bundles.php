84b07f244c984805b9e9e8122265a0c0428c3688
rahim <rahim.nagori@gmail.com>
2019-10-01 00:07:03 +0530

    Added Symfony Annotation package

--- agri_cast_api/config/bundles.php
+++ agri_cast_api/config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
